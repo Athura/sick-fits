58c3d5fa5c55c9773c38c49b7b0d77d551ee99e7
Athura <joshualjohnson33@gmail.com>
2018-10-17 07:47:34 -0400

    Finished setting up graphql + prisma connection

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,23 @@
-const Mutations = {};
+const Mutations = {
+    // ctx = context, we get this from createServer's context object
+    async createItem(parent, args, ctx, info) {
+        // TODO: Check if user is logged in
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+
+        return item;
+    }
+
+
+    // createDog(parent, args, ctx, info) {
+    //     global.dogs = global.dogs || [];
+    //     const newDog = { name: args.name };
+    //     global.dogs.push(newDog);
+    //     return newDog;
+    // }
+};
 
 module.exports = Mutations;
