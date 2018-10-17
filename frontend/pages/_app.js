bf91e8b8483c91892db196b21be392ffd964171b
Athura <joshualjohnson33@gmail.com>
2018-10-17 09:22:28 -0400

    Begin server query rendering. Some bugs on items.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,18 +1,34 @@
 import App, { Container } from 'next/app';
 import Page from '../components/Page';
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
 class MyApp extends App {
+    // this is a special next.js lifecycle method that will run before the first render occurs
+    static async getInitialProps({ Component, ctx }) {
+        let pageProps = {};
+        // This will look over every page that is loaded and call queries and mutations for us
+        if(Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
+        };
+        // This exposes the query to the user
+        pageProps.query = ctx.query;
+        return { pageProps };
+    }
+
     render() {
-        const { Component } = this.props;
+        const { Component, apollo, pageProps } = this.props;
 
         return (
             <Container>
-                <Page>
-                    <Component />
-                </Page>
+                <ApolloProvider client={apollo}>
+                    <Page>
+                        <Component {...pageProps} />
+                    </Page>
+                </ApolloProvider>
             </Container>
         )
     }
 }
 
-export default MyApp;
+export default withData(MyApp);
