f0e8470abbaf4c95a6eec3f635891608f99557f1
Athura <joshualjohnson33@gmail.com>
2018-10-25 07:49:25 -0400

    Begin user permission. Finished front end updating

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ const db = require("./db");
 const server = createServer();
 
 server.express.use(cookieParser());
+
 // decode the jwt so we can get the userId on each request
 server.express.use((req, res, next) => {
   const { token } = req.cookies;
@@ -16,7 +17,19 @@ server.express.use((req, res, next) => {
     req.userId = userId;
   }
   next();
-})
+});
+
+// Create a middleware that populates the user on each request
+server.express.use(async (req, res, next) => {
+  // if they aren't logged in, skip this
+  if (!req.userId) return next();
+  const user = await db.query.user(
+    { where: { id: req.userId } },
+    '{ id, permissions, email, name }'
+  );
+  req.user = user;
+  next();
+});
 
 server.start(
   {
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils');
 
 const Query = {
     items: forwardTo('db'),
@@ -12,6 +13,16 @@ const Query = {
         return ctx.db.query.user({
             where: { id: ctx.request.userId },
         }, info);
+    },
+    async users(parent, args, ctx, info) {
+        // Check if they're logged in
+        if(!ctx.request.userId) {
+            throw new Error('You must be logged in!');
+        }
+        // Check if the user has permissions to query all the users
+        hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+        // If they do, query all the users!
+        return ctx.db.query.users({}, info);
     }
 };
 
